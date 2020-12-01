f5ce5f5669d43d3e9fbc6fb50f6c8131b0fd8871
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2020-12-01 15:01:54 +0100

    12/1-2020
    gick bra lyckades använda toggle class för att gömma och vissa text

---
+++ 1st-form/form.js
@@ -0,0 +1,25 @@
+
+function Quiz() {
+}
+
+warning = "猫";
+somthing = "運動するのが大好きです";
+
+function AlertCat(){
+    window.alert(warning)
+}
+
+/* function writesomething() {
+    document.write(somthing)
+} */
+
+function writesomething() {
+    document.querySelector(".p").classList.toggle("invisible");
+    document.querySelector(".butt").classList.toggle("invisible");
+    document.getElementById("death").innerHTML = somthing;
+}
+
+function rehide() {
+    document.querySelector(".p").classList.toggle("invisible");
+    document.querySelector(".butt").classList.toggle("invisible");
+}
---
+++ quiz/quiz.js
@@ -0,0 +1,6 @@
+
+function myf() {
+    console.log(nihon);
+}
+nihon = "日本";
+myf();
